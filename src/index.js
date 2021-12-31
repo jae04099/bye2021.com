72d29eb8e78c9f000a21f5f7e1faf60d1a5722b5
deli-ght <minjju0660@gmail.com>
2021-12-31 17:45:01 +0900

    🎨 mk main to app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import App from "./App"
 import Main from "./pages/Main"
 import Answer from "./pages/Answer"
 import Question from "./pages/Question"
 import Result from "./pages/Result"
-import "./reset.css"
 
 ReactDOM.render(
   <BrowserRouter>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,43 +1,29 @@
-<<<<<<< HEAD
-import React from 'react'
-import FormName from '../components/Main/FormName'
-import styled from 'styled-components'
-import Button from '../components/Common/Button'
-
-export default function Main() {
-    return (
-        <>
-        <Header>안녕!<em>2021</em>✋</Header>
-            <FormName />
-            <Button toLink={'/question'} children={'2021 기록하기'}/>
-        </>
-    )
-}
-
-const Header = styled.h1`
-position: absolute;
-top: 145px;
-left: 50px;
-font-size: 36px;
-z-index: 1;
-  color: #222;
-  em {
-    font-style: normal;
-    color: #6D79EC;
-  }
-`
-=======
 import React from "react"
 import FormName from "../components/Main/FormName"
+import styled from "styled-components"
 import Button from "../components/Common/Button"
-import Container from "../components/Common/Container"
 
 export default function Main() {
   return (
-    <Container>
+    <>
+      <Header>
+        안녕!<em>2021</em>✋
+      </Header>
       <FormName />
-      <Button Button link={"/question"} children={"2021 기록하기"} />
-    </Container>
+      <Button toLink={"/question"} children={"2021 기록하기"} />
+    </>
   )
 }
->>>>>>> 25e2835 (🐛 fix error)
+
+const Header = styled.h1`
+  position: absolute;
+  top: 145px;
+  left: 50px;
+  font-size: 36px;
+  z-index: 1;
+  color: #222;
+  em {
+    font-style: normal;
+    color: #6d79ec;
+  }
+`
