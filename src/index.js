f89f624eedeef41c18f8bcc14959f792ee4a4eca
deli-ght <minjju0660@gmail.com>
2022-11-23 15:46:23 +0900

    💄 apply dnd layout style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import App from "./App"
-import Answer from "./pages/Answer"
-import Question from "./pages/Question"
-import Result from "./pages/Result"
-import "./reset.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Answer from "./pages/Answer";
+import Question from "./pages/Question";
+import Result from "./pages/Result";
+import "./reset.css";
+import "/node_modules/react-grid-layout/css/styles.css";
+import "/node_modules/react-resizable/css/styles.css";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -16,5 +18,5 @@ ReactDOM.render(
       <Route path="result" element={<Result />} />
     </Routes>
   </BrowserRouter>,
-  document.getElementById("root")
-)
+  document.getElementById("root"),
+);
