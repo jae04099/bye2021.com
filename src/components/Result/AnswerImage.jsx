30fe7d92886d156cdeabb9276e6ccafc2b085292
deli-ght <minjju0660@gmail.com>
2022-12-16 17:33:06 +0900

    🐛 이미지 오류 수정

--- src/components/Result/AnswerImage.jsx
+++ src/components/Result/AnswerImage.jsx
@@ -2,23 +2,46 @@ import styled from "styled-components";
 
 export const AnswerImage = ({ data }) => {
   const { pic_url, full_keyword } = data;
+
   return (
-    <Container className="masonry-item-image" bgImage={pic_url}>
+    <Container className="masonry-item-image">
       <Title>{full_keyword}</Title>
+      <MainImage className="my-images" src={pic_url} />
     </Container>
   );
 };
 
 const Container = styled.div`
+  position: relative;
   padding: 10px 11px;
-  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0)),
-    url(${({ bgImage }) => bgImage}) center center no-repeat;
-  background-size: cover;
   border-radius: 15px;
   color: #ffffff;
+  overflow: hidden;
+
+  &::after {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0));
+  }
 `;
 
 const Title = styled.span`
+  position: relative;
   margin: 0;
   font-weight: 700;
+  z-index: 100;
+`;
+
+const MainImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
---
+++ src/components/Result/ImageModal.jsx
@@ -0,0 +1,12 @@
+export const ImageModal = () => {
+  return (
+    <section>
+      <h1>
+        이규리의
+        <br />
+        2022년 순간들
+      </h1>
+      <article></article>
+    </section>
+  );
+};
