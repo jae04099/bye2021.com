a48ec15ad724263c1e34bea3e510a49b302b136c
deli-ght <minjju0660@gmail.com>
2022-12-23 18:58:34 +0900

    🚑 렌더링 에러 수정

--- src/App.js
+++ src/App.js
@@ -11,50 +11,47 @@ export default function App() {
       alert("링크가 복사되었습니다! 즐거운 한해 마무리 하세요~");
     });
   };
-  window.onload = function () {
-    if (
-      navigator.userAgent.match(
-        /inapp|NAVER|KAKAOTALK|Snapchat|Line|WirtschaftsWoche|Thunderbird|Instagram|everytimeApp|WhatsApp|Electron|wadiz|AliApp|zumapp|iPhone(.*)Whale|Android(.*)Whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|SamsungBrowser\/[^1]/i,
-      )
-    ) {
-      document.body.innerHTML = "";
-      if (navigator.userAgent.match(/iPhone|iPad/i)) {
-        return (
-          <Container>
-            <Caution>
-              더 나은 환경을 위해 외부 브라우저로 이동해 이용해주세요! 🥲
-            </Caution>
-            <CopyButton type="button" onClick={copyUrl}>
-              링크 복사하기
-            </CopyButton>
-          </Container>
-        );
-      } else {
-        window.location.href =
-          "intent://" +
-          window.location.href.replace(/https?:\/\//i, "") +
-          "#Intent;scheme=http;package=com.android.chrome;end";
-      }
+  if (
+    navigator.userAgent.match(
+      /inapp|NAVER|KAKAOTALK|Snapchat|Line|WirtschaftsWoche|Thunderbird|Instagram|everytimeApp|WhatsApp|Electron|wadiz|AliApp|zumapp|iPhone(.*)Whale|Android(.*)Whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|SamsungBrowser\/[^1]/i,
+    )
+  ) {
+    if (navigator.userAgent.match(/iPhone|iPad/i)) {
+      return (
+        <Container>
+          <Caution>
+            더 나은 환경을 위해 외부 브라우저로 이동해 이용해주세요! 🥲
+          </Caution>
+          <CopyButton type="button" onClick={copyUrl}>
+            링크 복사하기
+          </CopyButton>
+        </Container>
+      );
+    } else {
+      window.location.href =
+        "intent://" +
+        window.location.href.replace(/https?:\/\//i, "") +
+        "#Intent;scheme=http;package=com.android.chrome;end";
     }
-  };
-
-  return (
-    <Container>
-      <Header>
-        안녕<span>2022</span>
-      </Header>
-      <Description>
-        <Strong>키워드</Strong>와 <Strong>색</Strong>으로
-        <br />
-        나의 2022 기록하기
-      </Description>
-      <Wrapper>
-        <BookImage src="/image/favorite-book.png" alt="" />
-        <FormName />
-        <Button toLink={"/answer"} children={"2022 정리하기"} />
-      </Wrapper>
-    </Container>
-  );
+  } else {
+    return (
+      <Container>
+        <Header>
+          안녕<span>2022</span>
+        </Header>
+        <Description>
+          <Strong>키워드</Strong>와 <Strong>색</Strong>으로
+          <br />
+          나의 2022 기록하기
+        </Description>
+        <Wrapper>
+          <BookImage src="/image/favorite-book.png" alt="" />
+          <FormName />
+          <Button toLink={"/answer"} children={"2022 정리하기"} />
+        </Wrapper>
+      </Container>
+    );
+  }
 }
 
 const Header = styled.h1`
