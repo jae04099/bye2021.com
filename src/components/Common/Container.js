63a020dadf4ba13cf0a92ee25b036963f500734d
gyuri <jae04099@catholic.ac.kr>
2022-12-24 22:51:38 +0900

    :fire: remove bg pink

--- src/components/Common/Container.js
+++ src/components/Common/Container.js
@@ -4,10 +4,8 @@ const Container = styled.main`
   max-width: 500px;
   box-sizing: border-box;
   height: 100vh;
-  /* height: -webkit-fill-available; */
   margin: 0 auto;
   padding: 50px 20px 0;
-  background: pink;
 `;
 
 export default Container;
